0ddabd91eadced5cadea1fd028226972edb7a5f7
Ascher <atownsend3@madisoncollege.edu>
2023-04-16 18:12:55 -0500

    Working on the setup page

--- src/Setup.tsx
+++ src/Setup.tsx
@@ -1,5 +1,7 @@
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import { SetupData } from "./data-models";
+import { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 
 const radios = [
@@ -12,20 +14,33 @@ const radios = [
 export const Setup = () => {
 
     const nav = useNavigate();
+    const [numberOfPlayers, setNumberOfPlayers] = useState(3);
+
+    const startGame = () => {
+        
+
+
+        nav("/results");
+    };
 
     return (
-        <>
-            <h1>Setup</h1>
-            <h2>Please select the number of players:</h2>
-            <Form>
-                {radios.map((radio) => (
-                    <Form.Check type='radio'
-                    name='radios'
-                    id={radio.name}
-                    label={radio.value}/>
-                ))}
-            </Form>
-            <Button onClick={() => nav("/results")}>Start</Button>
-        </>
+        <div className="d-flex align-items-center justify-content-center">
+            <div>
+                <h1>Setup</h1>
+                <h2>Please select the number of players:</h2>
+                <Form className="mb-3">
+                    {radios.map((radio, index) => (
+                        <Form.Check type='radio'
+                        name='radios'
+                        id={radio.name}
+                        label={radio.name}
+                        value={radio.value}
+                        key={index}
+                        onChange={e => setNumberOfPlayers(Number(e.target.value))}/>
+                    ))}
+                </Form>
+                <Button onClick={startGame}>Start</Button>
+            </div>
+        </div>
     );
 };
